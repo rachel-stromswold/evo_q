a7fea7a05155a70659e79954785afba8472c55a7
sam-stromswold <samstromsw@gmail.com>
2019-08-06 17:54:42 -0700

    fixed convergence.hpp to match

--- include/evo_q/convergence.hpp
+++ include/evo_q/convergence.hpp
@@ -5,29 +5,26 @@ namespace Genetics {
 
 class Conv_VarianceCutoff : public ConvergenceCriteria {
   private:
-    _uint N_OBJS;
     double cutoff;
 
     Vector<double> max_variance;//used for normalization
   public:
-    Conv_VarianceCutoff(_uint N_OBJS, double p_cutoff);
-    bool evaluate_convergence(FitnessStats* stats);
+    Conv_VarianceCutoff(double p_cutoff);
+    bool evaluate_convergence(_uint N_OBJS, FitnessStats* stats);
 };
 
 class Conv_RangeCutoff : public ConvergenceCriteria {
   private:
-    _uint N_OBJS;
     double cutoff;
 
     Vector<double> max_range;//used for normalization
   public:
-    Conv_RangeCutoff(_uint N_OBJS, double p_cutoff);
-    bool evaluate_convergence(FitnessStats* stats);
+    Conv_RangeCutoff(double p_cutoff);
+    bool evaluate_convergence(_uint N_OBJS, FitnessStats* stats);
 };
 
 class Conv_Plateau : public ConvergenceCriteria {
   private:
-    _uint N_OBJS;
     double fitness_threshold;
     _uint generation_cutoff;
     _uint gens_without_improvement;
@@ -35,39 +32,36 @@ class Conv_Plateau : public ConvergenceCriteria {
     Vector<double> max_fitness;//used for normalization
     Vector<double> prev_fitness;
   public:
-    Conv_Plateau(_uint N_OBJS, double p_fitness_threshold, _uint p_generation_cutoff);
-    bool evaluate_convergence(FitnessStats* stats);
+    Conv_Plateau(double p_fitness_threshold, _uint p_generation_cutoff);
+    bool evaluate_convergence(_uint N_OBJS, FitnessStats* stats);
 };
 
 class uConv_VarianceCutoff : public ConvergenceCriteria {
   private:
-    _uint N_OBJS;
     Vector<double> cutoffs;
   public:
-    uConv_VarianceCutoff(_uint N_OBJS, Vector<double> p_cutoffs);
-    bool evaluate_convergence(FitnessStats* stats);
+    uConv_VarianceCutoff(Vector<double> p_cutoffs);
+    bool evaluate_convergence(_uint N_OBJS, FitnessStats* stats);
 };
 
 class uConv_RangeCutoff : public ConvergenceCriteria {
   private:
-    _uint N_OBJS;
     Vector<double> cutoffs;
   public:
-    uConv_RangeCutoff(_uint N_OBJS, Vector<double> p_cutoffs);
-    bool evaluate_convergence(FitnessStats* stats);
+    uConv_RangeCutoff(Vector<double> p_cutoffs);
+    bool evaluate_convergence(_uint N_OBJS, FitnessStats* stats);
 };
 
 class uConv_Plateau : public ConvergenceCriteria {
   private:
-    _uint N_OBJS;
     _uint generation_cutoff;
     _uint gens_without_improvement;
 
     Vector<double> fitness_thresholds;
     Vector<double> prev_fitness;
   public:
-    uConv_Plateau(_uint N_OBJS, Vector<double> p_fitness_thresholds, _uint p_generation_cutoff);
-    bool evaluate_convergence(FitnessStats* stats);
+    uConv_Plateau(Vector<double> p_fitness_thresholds, _uint p_generation_cutoff);
+    bool evaluate_convergence(_uint N_OBJS, FitnessStats* stats);
 };
 
 }
