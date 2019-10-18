6052722e9482230f21ac382ada6a9c87b33ae278
Stromswold <samuel.stromswold@nasa.gov>
2019-10-17 18:32:40 -0700

    fixed evo_q.hpp header

--- include/evo_q/evo_q.hpp
+++ include/evo_q/evo_q.hpp
@@ -64,6 +64,15 @@
 #define FLAG_DIST_SET	2
 #define FLAG_BEST_FOUND	4
 #define FLAG_FRONTS	8
+#if __cplusplus >= 201402L
+#define DEPRECATED(msg) [[ deprecated(msg) ]]
+#elif defined(__GNUC__)
+#define DEPRECATED(msg) __attribute__ ((deprecated(msg)))
+#elif defined(_MSC_VER)
+#define DEPRECATED(msg) __declspec(deprecated(msg))
+#else
+#define DEPRECATED(msg) 
+#endif
 
 namespace Genetics {
 //UTIL_H
